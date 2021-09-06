3f372df96c3db8b98329727122e14105b3b49c7e
Megan Hoang <meganthoang@gmail.com>
2021-09-05 23:54:32 -0400

    testing git commit

---
+++ test.r
@@ -0,0 +1,13 @@
+# Uber Analytics Project
+# Megan H., Raheel Q., & Sawmon A.
+# 9/5/2021
+
+library(boot)
+library(dplyr)
+library(caret)
+library(ROCR)
+library(tidyverse)
+library(haven)
+library(statar)
+library(pROC)
+library(readr)
