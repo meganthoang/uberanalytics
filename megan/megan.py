5cc8cf6c7d00daa74f8df0ee39b4483ab1630690
meganthoang <77313353+meganthoang@users.noreply.github.com>
2021-09-07 18:16:41 -0400

    edited megan.py

--- megan/megan.py
+++ megan/megan.py
@@ -1,1 +1,15 @@
 # Megan's Code
+
+# Import necessary modules
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import os
+
+# %matplotlib inline -- for Jupyter Notebook
+
+# Find out the current directory.
+os.getcwd()
+# print (os.getcwd()) -- prints "C:\Users\megan\OneDrive\Desktop\Analytics\uberanalytics\megan"
