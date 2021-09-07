e3473b1c9907f2603b8f9d9465345f2cfa71836c
Sawmonabo <77422313+Sawmonabo@users.noreply.github.com>
2021-09-07 17:45:31 -0400

    update

---
+++ sawmon.py
@@ -0,0 +1,15 @@
+# Import necessary modules
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import os
+
+# %matplotlib inline -- for jupyter only
+
+print(os.getcwd())
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
