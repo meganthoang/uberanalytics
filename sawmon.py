d2199e203e201386316277d236a295cefced41b7
Sawmonabo <77422313+Sawmonabo@users.noreply.github.com>
2021-09-07 17:09:55 -0400

    Create sawmon.py
    Sawmon's Uber Analytics Code

---
+++ sawmon.py
@@ -0,0 +1,22 @@
+# Import necessary modules
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import os
+
+'''
+python -m pip install matplotlib
+python -m pip install pandas
+python -m pip install scikit-learn
+python -m pip install matplotlib
+python -m pip install jupyter
+'''
+
+print("help")
+
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
+# python -m pip install <package>
