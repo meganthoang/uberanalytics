ce06eb5a894411028d241933ab4fedfabd1c1a66
Sawmonabo <77422313+Sawmonabo@users.noreply.github.com>
2021-09-07 17:14:08 -0400

    Update sawmon.py

--- sawmon.py
+++ sawmon.py
@@ -7,15 +7,6 @@ from matplotlib import pyplot as plt
 import glob
 import os
 
-'''
-python -m pip install matplotlib
-python -m pip install pandas
-python -m pip install scikit-learn
-python -m pip install matplotlib
-python -m pip install jupyter
-'''
-
-print("help")
 
 # curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # python get-pip.py
