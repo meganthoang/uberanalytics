bb53e7bcad59c40c74bd530db5061d24d0763104
meganthoang <77313353+meganthoang@users.noreply.github.com>
2021-09-09 03:06:39 -0400

    updated meg's code

--- megan/megan.py
+++ megan/megan.py
@@ -8,8 +8,14 @@ from matplotlib import pyplot as plt
 import glob
 import os
 
-# %matplotlib inline -- for Jupyter Notebook
+# %% matplotlib inline -- for Jupyter Notebook
 
 # Find out the current directory.
 os.getcwd()
 # print (os.getcwd()) -- prints "C:\Users\megan\OneDrive\Desktop\Analytics\uberanalytics\megan"
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+# still need to change this directory to match the data folder
+
+
